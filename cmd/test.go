2a855612c8619c921774e99c521f5e808bb5f69a
664553716@qq.com <664553716@qq.com>
2019-05-26 19:41:49 +0800

    test

--- cmd/test.go
+++ cmd/test.go
@@ -1,11 +1,18 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
+	"time"
+
 	//"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
 	"iads/lib/linux"
-	"iads/server/api_v1_0"
+	_ "iads/server/api_v1_0"
 )
 
 func init() {
@@ -38,7 +45,37 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "run roycom common server test",
 	Run: func(cmd *cobra.Command, args []string) {
-		k8s := api_v1_0.K8sOp{}
-		fmt.Println(k8s.GetNodeInfo())
+		var kubeconfig *string
+		if home := homeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
+		// uses the current context in kubeconfig
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			panic(err.Error())
+		}
+		// creates the clientset
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err.Error())
+		}
+		for {
+			pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+			if err != nil {
+				panic(err.Error())
+			}
+			fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+			time.Sleep(10 * time.Second)
+		}
 	},
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
--- server/api_v1_0/kubernetes.go
+++ server/api_v1_0/kubernetes.go
@@ -6,24 +6,31 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 	"log"
+	"os"
 	"path/filepath"
 )
 
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
 type K8sOp struct {
-	kubeconfig *string
-	clientset  *kubernetes.Clientset
+	clientset *kubernetes.Clientset
 }
 
 func (e *K8sOp) Init() {
-	if home := homedir.HomeDir(); home != "" {
-		e.kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		e.kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *e.kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
