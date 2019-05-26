a48ab5d101595d0d91b3fbe0215245b0ea422b44
664553716@qq.com <664553716@qq.com>
2019-05-26 19:46:31 +0800

    test

--- cmd/test.go
+++ cmd/test.go
@@ -1,18 +1,11 @@
 package cmd
 
 import (
-	"flag"
 	"fmt"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
-	"time"
-
 	//"github.com/jedib0t/go-pretty/table"
 	"github.com/spf13/cobra"
 	"iads/lib/linux"
-	_ "iads/server/api_v1_0"
+	"iads/server/api_v1_0"
 )
 
 func init() {
@@ -45,37 +38,8 @@ var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "run roycom common server test",
 	Run: func(cmd *cobra.Command, args []string) {
-		var kubeconfig *string
-		if home := homeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-		flag.Parse()
-		// uses the current context in kubeconfig
-		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		if err != nil {
-			panic(err.Error())
-		}
-		// creates the clientset
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			panic(err.Error())
-		}
-		for {
-			pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-			time.Sleep(10 * time.Second)
-		}
+		k8s := api_v1_0.K8sOp{}
+		k8s.Init()
+		fmt.Println(k8s.GetNodeInfo())
 	},
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
 }
