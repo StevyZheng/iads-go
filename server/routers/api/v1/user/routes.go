ac00edbb07b432eaae96214d5b9ce33972161500
664553716@qq.com <664553716@qq.com>
2019-06-14 00:01:39 +0800

    201906121802

--- server/routers/api/v1/user/routes.go
+++ server/routers/api/v1/user/routes.go
@@ -1,9 +1,11 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-// RegisterRouter 注册路由
-func RegisterRouter(r *gin.RouterGroup) {
+// 用户RegisterRouter 注册路由
+func RegisterUserRouter(r *gin.RouterGroup) {
 
 	// 注册
 	r.POST("/register", register)
@@ -21,5 +23,21 @@ func RegisterRouter(r *gin.RouterGroup) {
 		auth.DELETE("/:id", DeleteUserByID)
 		// 更新用户信息
 		auth.PUT("/:id", UpdateUserByID)
+	}
+}
+
+// 角色RegisterRouter 注册路由
+func RegisterRoleRouter(r *gin.RouterGroup) {
+	auth := r.Group("")
+	auth.Use(Auth.MiddlewareFunc())
+	{
+		// 角色列表
+		auth.GET("/list", RoleList)
+		//添加角色
+		auth.POST("/add", AddRole)
+		// 删除角色
+		auth.DELETE("/:id", DeleteRoleByID)
+		// 更新角色信息
+		auth.PUT("/:id", UpdateRoleByID)
 	}
 }
